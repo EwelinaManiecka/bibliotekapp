8ff9823cbff5bcdf223d7e859962a669844e2fa8
EwelinaManiecka <emaniecka3@gmail.com>
2024-06-30 20:50:30 +0200

    add style and database first to project

---
+++ src/components/Header.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Header = () => {
+    return (
+        <section>
+<header className="bg-white">
+<nav className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4 bg-gray-900">
+      <div className="bg-gray-900 p-4 text-green-50">O mnie</div>
+      <div className="bg-gray-900 p-4 text-green-50">Książki</div>
+    </nav>
+</header>
+
+        </section>
+    )
+};
+
+export default Header;
