250a246bdb8d1361f4965565b77d357a545640a4
EwelinaManiecka <emaniecka3@gmail.com>
2024-07-03 17:45:51 +0200

    add component add book with problems

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
+import React, {useState} from 'react';
+
 import './App.css';
 import Header from './components/Header';
 import Button from './common/Button';
 import Books from './components/Books';
+import AddBook from './components/AddBook';
+
 
 function App() {
+  const [books, setBooks] = useState([]);
+
+  const handleAddBook = (newBook) => {
+    setBooks(prevBooks => [...prevBooks, newBook]);
+  }
+
   return (
     <div className="App">
       <Header />
-      <Books />
+      <AddBook onAdd={handleAddBook}/>
+      <Books books={books}/>
       <Button />
     </div>
   );
